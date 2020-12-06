046f1ed124ec9143ee3738d8c1233edb105243da
charanjit ghai <ghaicharanjit@gmail.com>
2020-12-05 20:48:10 -0800

    added support for messages

---
+++ doc_rank/src/lexer.py
@@ -0,0 +1,37 @@
+import nltk
+
+        
+class lexer:
+
+    def __init__(self, stem=True, ignore_stop_word=True, debug=False):
+        self.ignore_stop_word = ignore_stop_word
+        self.stop_word_list = {'i':'1', 'me':'1', 'my':'1', 'myself':'1', 'we':'1', 'or':'1', 'ors':'1', 'orselves':'1', 'yo':'1', 'yor':'1', 'yors':'1', 'yorself':'1', 'yorselves':'1', 'he':'1', 'him':'1', 'his':'1', 'himself':'1', 'she':'1', 'her':'1', 'hers':'1', 'herself':'1', 'it':'1', 'its':'1', 'itself':'1', 'they':'1', 'them':'1', 'their':'1', 'theirs':'1', 'themselves':'1', 'what':'1', 'which':'1', 'who':'1', 'whom':'1', 'this':'1', 'that':'1', 'these':'1', 'those':'1', 'am':'1', 'is':'1', 'are':'1', 'was':'1', 'were':'1', 'be':'1', 'been':'1', 'being':'1', 'have':'1', 'has':'1', 'had':'1', 'having':'1', 'do':'1', 'does':'1', 'did':'1', 'doing':'1', 'a':'1', 'an':'1', 'the':'1', 'and':'1', 'bt':'1', 'if':'1', 'or':'1', 'becase':'1', 'as':'1', 'ntil':'1', 'while':'1', 'of':'1', 'at':'1', 'by':'1', 'for':'1', 'with':'1', 'abot':'1', 'against':'1', 'between':'1', 'into':'1', 'throgh':'1', 'dring':'1', 'before':'1', 'after':'1', 'above':'1', 'below':'1', 'to':'1', 'from':'1', 'p':'1', 'down':'1', 'in':'1', 'ot':'1', 'on':'1', 'off':'1', 'over':'1', 'nder':'1', 'again':'1', 'frther':'1', 'then':'1', 'once':'1', 'here':'1', 'there':'1', 'when':'1', 'where':'1', 'why':'1', 'how':'1', 'all':'1', 'any':'1', 'both':'1', 'each':'1', 'few':'1', 'more':'1', 'most':'1', 'other':'1', 'some':'1', 'sch':'1', 'no':'1', 'nor':'1', 'not':'1', 'only':'1', 'own':'1', 'same':'1', 'so':'1', 'than':'1', 'too':'1', 'very':'1', 's':'1', 't':'1', 'can':'1', 'will':'1', 'jst':'1', 'don':'1', 'shold':'1', 'now':'1'}
+        self.stem = stem
+        self.word_stemmer = nltk.stem.porter.PorterStemmer()
+        self.debug = debug
+
+    def log(self, *arg):
+        if self.debug:
+            print(arg)
+    
+    def is_valid_word(self, word):
+        word = word.lower()
+        if self.stem:
+            word = self.word_stemmer.stem(word)
+        if word.isalpha():
+            if self.ignore_stop_word and (word in self.stop_word_list):
+                return False, ""
+            return True, word
+        return False, ""
+
+    def get_all_words(self, text):
+        self.log("attempting to get words")
+        words = text.split()
+        final_words = [] 
+        for word in words:
+            (valid, final_word) = self.is_valid_word(word)
+            if valid:
+                final_words.append(final_word)
+        self.log("exiting with " + ",".join(final_words))
+        return final_words
+
--- doc_rank/src/pl.py
+++ doc_rank/src/pl.py
@@ -1,7 +1,8 @@
 from os import path
 from pathlib import Path
 import numpy as np
-from parser import HtmlParser
+from parser import parser
+from lexer import lexer
 
 class pl_entry:
 	def __init__(self, entity_id, tf):
@@ -13,14 +14,21 @@ class pl_entry:
 
 
 class pl:
-	def __init__(self, data_dir):
+	def __init__(self, data_dir, debug=False):
 		self.wf_path = data_dir + "wf.npy"
 		Path(data_dir).mkdir(parents=True, exist_ok=True)
 		self.word_to_entries = np.load(self.wf_path, allow_pickle='TRUE').item() if path.exists(self.wf_path) else {}
+		self.parser = parser(debug=debug)
+		self.lexer = lexer(debug=debug)
+		self.debug = debug
+
+	def log(self, *arg):
+		if self.debug:
+			print(arg)
 
 	def add_entry(self, word, entity_id, tf):
-		wple = self.word_to_entries[word] if word in self.word_to_entries else []
-		wple.append(pl_entry(entity_id, tf))
+		wple = self.word_to_entries[word] if word in self.word_to_entries else {}
+		wple[entity_id] = pl_entry(entity_id, tf)
 		self.word_to_entries[word] = wple
 
 	def process(self, entity_id, words):
@@ -30,19 +38,23 @@ class pl:
 				wm[word] = 0
 			wm[word] += 1
 		for word in wm:
+			self.log("adding entry ", word, ", ", wm[word])
 			self.add_entry(word, entity_id, wm[word])
 
 	def process_doc(self, url):
-		parser = HtmlParser(url)
-		words = parser.get_all_words()
+		text = self.parser.get_text(url)
+		words = self.lexer.get_all_words(text)
 		self.process(url, words)
+
+	def process_msg(self, msg_id, text):
+		self.process(msg_id, self.lexer.get_all_words(text))
 
 	def __str__(self):
 		str = ""
 		for word in self.word_to_entries:
 			str = str + word + ":["
 			for entry in self.word_to_entries[word]:
-				str += ("{" + entry.__str__() + "},")
+				str += ("{" + entry + "},")
 			str += "]"
 			str += "\n"
 		return str
@@ -50,13 +62,45 @@ class pl:
 	def close(self):
 		np.save(self.wf_path, self.word_to_entries)
 
+	def rank(self, keywords, limit):
+		doc_to_score = {}
+		for keyword in keywords:
+			for (_, entry) in self.word_to_entries[keyword].items():
+				doc = entry.entity_id
+				score = entry.tf
+				if doc not in doc_to_score:
+					doc_to_score[doc] = 0
+				doc_to_score[doc] += score
+		doc_score_tups = [(word, score) for (word, score) in doc_to_score.items()]
+		doc_score_tups = sorted(doc_score_tups, key = lambda x : x[1])
+		top = doc_score_tups[-limit:]
+		return top
 
-pli = pl('./data/')
-pli.process_doc("http://www.google.com")
-print("pli from memory")
-print(pli)
-pli.close()
-pli = pl('./data/')
-pli.process_doc("http://www.youtube.com")
-print("pli after reading")
-print(pli)
+
+def test():
+	pli = pl('./data/',debug=True)
+	pli.process_doc("http://www.youtube.com")
+	pli.process_doc("http://www.google.com")
+	top2 = pli.rank(['new', 'map'], 2)
+	for url in ["http://www.youtube.com", "http://www.google.com"]:
+		present = False
+		for entry in top2:
+			if url in entry[0]:
+				present = True
+		if not present:
+			print("Failed")
+			return -1
+	print("Passed")
+
+def test2():
+	pli = pl('./data/', debug=True)
+	pli.process_doc("http://www.youtube.com")
+	pli.process_doc("http://www.google.com")
+	pli.process_msg("m1", "news map news")
+	top = pli.rank(['news', 'map'], 2)[-1]
+	if top[0] != "m1":
+		print("Failed: expected m1 was ", top[0])
+		return -1
+	print("Passed")	
+test()
+test2()
